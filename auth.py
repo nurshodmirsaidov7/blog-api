338448a5e43c0889bffee5b2b07c4fe203c2ff40
Your Name <you@example.com>
2026-04-27 11:58:12 +0500

    get_current_user functiion is added on auth.py

--- auth.py
+++ auth.py
@@ -1,3 +1,8 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from database import get_db
+import models
 from passlib.context import CryptContext
 from jose import jwt
 from datetime import datetime, timedelta
@@ -7,6 +12,9 @@ ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/login')
+
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
@@ -19,4 +27,18 @@ def create_access_token(data: dict) -> str:
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({'exp': expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get('sub')
+        if email is None:
+            raise HTTPException(status_code=401, detail='Invalid token!')
+    except: 
+            raise HTTPException(status_code=401, detail='Invalid token!')
+
+    user = db.query(models.User).filter(models.User.email == email).first()
+    if user is None:
+            raise HTTPException(status_code=401, detail='User not found')
+    return user 
+        
