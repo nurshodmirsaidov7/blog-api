5dc3d0f86b6e31fd8d96bc525ab9fa7f284bd04a
Your Name <you@example.com>
2026-04-28 11:23:59 +0500

    move secrets to environment variables

--- auth.py
+++ auth.py
@@ -6,9 +6,13 @@ import models
 from passlib.context import CryptContext
 from jose import jwt
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os
 
-SECRET_KEY = 'my-secret-key'
-ALGORITHM = 'HS256'
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
--- database.py
+++ database.py
@@ -1,8 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = 'postgresql://postgres:nurshoddeveloper7@localhost:5432/blogapi_db'
+load_dotenv()
+
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
 
 engine = create_engine(DATABASE_URL)
 
