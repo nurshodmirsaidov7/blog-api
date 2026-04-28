aa9504b2cbfd34239aaaea9014747c9a3755dca4
Your Name <you@example.com>
2026-04-28 12:21:42 +0500

    create uploads directory on startup

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from database import engine
-import models
+import models, os
 from routers import auth, posts, comments
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+os.makedirs("uploads", exist_ok=True)
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.include_router(auth.router)
